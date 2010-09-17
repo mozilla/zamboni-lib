468ea188d9e9a14f3a35c439195a9d04ccdc3665
Jeff Balogh <jbalogh@mozilla.com>
2010-09-17 13:31:12 -0700

    adding the basta validator

---
+++ packages/cssutils/setup.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+"""
+cssutils setup
+
+use EasyInstall or install with
+    >python setup.py install
+"""
+__docformat__ = 'restructuredtext'
+__author__ = 'Christof Hoeke with contributions by Walter Doerwald'
+__date__ = '$LastChangedDate:: 2010-05-23 22:02:28 +0200 #$:'
+
+import codecs
+import os
+import sys
+sys.path.insert(0, "src")
+from cssutils import VERSION
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+def read(*rnames):
+    return codecs.open(os.path.join(*rnames), encoding='utf-8').read()
+
+long_description = u'\n' + read('README.txt') + u'\n'# + read('CHANGELOG.txt')
+
+setup(
+    name='cssutils',
+    version=VERSION,
+    package_dir={'':'src'},
+    packages=find_packages('src', exclude=('tests*',)),
+    test_suite='tests', #'nose.collector'
+    tests_require='minimock',
+    entry_points={
+        'console_scripts': [
+            'csscapture = cssutils.scripts.csscapture:main',
+            'csscombine = cssutils.scripts.csscombine:main',
+            'cssparse = cssutils.scripts.cssparse:main'
+        ]
+    },
+    description='A CSS Cascading Style Sheets library for Python',
+    long_description=long_description,
+    author='Christof Hoeke',
+    author_email='c@cthedot.de',
+    url='http://cthedot.de/cssutils/',
+    download_url='http://code.google.com/p/cssutils/downloads/list',
+    license='LGPL 2.1 or later, see also http://cthedot.de/cssutils/',
+    keywords='CSS, Cascading Style Sheets, CSSParser, DOM Level 2 Stylesheets, DOM Level 2 CSS',
+    platforms='Python 2.4 and later.',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML'
+        ]
+    )
