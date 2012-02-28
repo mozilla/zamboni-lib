e4574522037c309196681119ab76cd59f6adfa58
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-02-27 21:03:02 -0600

    Upgrade nose to 1.1.3 (source head)
    From git @ a014b52003813b92563946329b93cf31cb32eb3d
    This is mainly to pick up the --cover-xml flag

--- lib/python/nose/plugins/cover.py
+++ lib/python/nose/plugins/cover.py
@@ -11,43 +11,12 @@ variable.
 .. _coverage: http://www.nedbatchelder.com/code/modules/coverage.html
 """
 import logging
-import os
 import re
 import sys
 from nose.plugins.base import Plugin
 from nose.util import src, tolist
 
-log =  logging.getLogger(__name__)
-
-COVERAGE_TEMPLATE = '''<html>
-<head>
-%(title)s
-</head>
-<body>
-%(header)s
-<style>
-.coverage pre {float: left; margin: 0px 1em; border: none;
-               padding: 0px; }
-.num pre { margin: 0px }
-.nocov, .nocov pre {background-color: #faa}
-.cov, .cov pre {background-color: #cfc}
-div.coverage div { clear: both; height: 1.1em}
-</style>
-<div class="stats">
-%(stats)s
-</div>
-<div class="coverage">
-%(body)s
-</div>
-</body>
-</html>
-'''
-
-COVERAGE_STATS_TEMPLATE = '''Covered: %(covered)s lines<br/>
-Missed: %(missed)s lines<br/>
-Skipped %(skipped)s lines<br/>
-Percent: %(percent)s %%<br/>
-'''
+log = logging.getLogger(__name__)
 
 
 class Coverage(Plugin):
@@ -56,25 +25,16 @@ class Coverage(Plugin):
     """
     coverTests = False
     coverPackages = None
-    _coverInstance = None
+    coverInstance = None
+    coverErase = False
     score = 200
     status = {}
-
-    def coverInstance(self):
-        if not self._coverInstance:
-            import coverage
-            try:
-                self._coverInstance = coverage.coverage()
-            except coverage.CoverageException:
-                self._coverInstance = coverage
-        return self._coverInstance
-    coverInstance = property(coverInstance)
 
     def options(self, parser, env):
         """
         Add options to command line.
         """
-        Plugin.options(self, parser, env)
+        super(Coverage, self).options(parser, env)
         parser.add_option("--cover-package", action="append",
                           default=env.get('NOSE_COVER_PACKAGE'),
                           metavar="PACKAGE",
@@ -108,8 +68,22 @@ class Coverage(Plugin):
                           dest='cover_html_dir',
                           metavar='DIR',
                           help='Produce HTML coverage information in dir')
+        parser.add_option("--cover-branches", action="store_true",
+                          default=env.get('NOSE_COVER_BRANCHES'),
+                          dest="cover_branches",
+                          help="Include branch coverage in coverage report "
+                          "[NOSE_COVER_BRANCHES]")
+        parser.add_option("--cover-xml", action="store_true",
+                          default=env.get('NOSE_COVER_XML'),
+                          dest="cover_xml",
+                          help="Produce XML coverage information")
+        parser.add_option("--cover-xml-file", action="store",
+                          default=env.get('NOSE_COVER_XML_FILE', 'coverage.xml'),
+                          dest="cover_xml_file",
+                          metavar="FILE",
+                          help="Produce XML coverage information in file")
 
-    def configure(self, options, config):
+    def configure(self, options, conf):
         """
         Configure plugin.
         """
@@ -117,8 +91,8 @@ class Coverage(Plugin):
             self.status.pop('active')
         except KeyError:
             pass
-        Plugin.configure(self, options, config)
-        if config.worker:
+        super(Coverage, self).configure(options, conf)
+        if conf.worker:
             return
         if self.enabled:
             try:
@@ -128,7 +102,7 @@ class Coverage(Plugin):
                           "unable to import coverage module")
                 self.enabled = False
                 return
-        self.conf = config
+        self.conf = conf
         self.coverErase = options.cover_erase
         self.coverTests = options.cover_tests
         self.coverPackages = []
@@ -143,8 +117,15 @@ class Coverage(Plugin):
         if options.cover_html:
             self.coverHtmlDir = options.cover_html_dir
             log.debug('Will put HTML coverage report in %s', self.coverHtmlDir)
+        self.coverBranches = options.cover_branches
+        self.coverXmlFile = None
+        if options.cover_xml:
+            self.coverXmlFile = options.cover_xml_file
+            log.debug('Will put XML coverage report in %s', self.coverXmlFile)
         if self.enabled:
             self.status['active'] = True
+            self.coverInstance = coverage.coverage(auto_data=False,
+                branch=self.coverBranches, data_suffix=None)
 
     def begin(self):
         """
@@ -154,8 +135,10 @@ class Coverage(Plugin):
         self.skipModules = sys.modules.keys()[:]
         if self.coverErase:
             log.debug("Clearing previously collected coverage statistics")
+            self.coverInstance.combine()
             self.coverInstance.erase()
         self.coverInstance.exclude('#pragma[: ]+[nN][oO] [cC][oO][vV][eE][rR]')
+        self.coverInstance.load()
         self.coverInstance.start()
 
     def report(self, stream):
@@ -164,107 +147,19 @@ class Coverage(Plugin):
         """
         log.debug("Coverage report")
         self.coverInstance.stop()
+        self.coverInstance.combine()
         self.coverInstance.save()
-        modules = [ module
+        modules = [module
                     for name, module in sys.modules.items()
-                    if self.wantModuleCoverage(name, module) ]
+                    if self.wantModuleCoverage(name, module)]
         log.debug("Coverage report will cover modules: %s", modules)
         self.coverInstance.report(modules, file=stream)
         if self.coverHtmlDir:
             log.debug("Generating HTML coverage report")
-            if hasattr(self.coverInstance, 'html_report'):
-                self.coverInstance.html_report(modules, self.coverHtmlDir)
-            else:
-                self.report_html(modules)
-
-    def report_html(self, modules):
-        if not os.path.exists(self.coverHtmlDir):
-            os.makedirs(self.coverHtmlDir)
-        files = {}
-        for m in modules:
-            if hasattr(m, '__name__') and hasattr(m, '__file__'):
-                files[m.__name__] = m.__file__
-        self.coverInstance.annotate(files.values())
-        global_stats =  {'covered': 0, 'missed': 0, 'skipped': 0}
-        file_list = []
-        for m, f in files.iteritems():
-            if f.endswith('pyc'):
-                f = f[:-1]
-            coverfile = f+',cover'
-            outfile, stats = self.htmlAnnotate(m, f, coverfile,
-                                               self.coverHtmlDir)
-            for field in ('covered', 'missed', 'skipped'):
-                global_stats[field] += stats[field]
-            file_list.append((stats['percent'], m, outfile, stats))
-            os.unlink(coverfile)
-        file_list.sort()
-        global_stats['percent'] = self.computePercent(
-            global_stats['covered'], global_stats['missed'])
-        # Now write out an index file for the coverage HTML
-        index = open(os.path.join(self.coverHtmlDir, 'index.html'), 'w')
-        index.write('<html><head><title>Coverage Index</title></head>'
-                    '<body><p>')
-        index.write(COVERAGE_STATS_TEMPLATE % global_stats)
-        index.write('<table><tr><td>File</td><td>Covered</td><td>Missed'
-                    '</td><td>Skipped</td><td>Percent</td></tr>')
-        for junk, name, outfile, stats in file_list:
-            stats['a'] = '<a href="%s">%s</a>' % (outfile, name)
-            index.write('<tr><td>%(a)s</td><td>%(covered)s</td><td>'
-                        '%(missed)s</td><td>%(skipped)s</td><td>'
-                        '%(percent)s %%</td></tr>' % stats)
-        index.write('</table></p></html')
-        index.close()
-
-    def htmlAnnotate(self, name, file, coverfile, outputDir):
-        log.debug('Name: %s file: %s' % (name, file, ))
-        rows = []
-        data = open(coverfile, 'r').read().split('\n')
-        padding = len(str(len(data)))
-        stats = {'covered': 0, 'missed': 0, 'skipped': 0}
-        for lineno, line in enumerate(data):
-            lineno += 1
-            if line:
-                status = line[0]
-                line = line[2:]
-            else:
-                status = ''
-                line = ''
-            lineno = (' ' * (padding - len(str(lineno)))) + str(lineno)
-            for old, new in (('&', '&amp;'), ('<', '&lt;'), ('>', '&gt;'),
-                             ('"', '&quot;'), ):
-                line = line.replace(old, new)
-            if status == '!':
-                rows.append('<div class="nocov"><span class="num"><pre>'
-                            '%s</pre></span><pre>%s</pre></div>' % (lineno,
-                                                                    line))
-                stats['missed'] += 1
-            elif status == '>':
-                rows.append('<div class="cov"><span class="num"><pre>%s</pre>'
-                            '</span><pre>%s</pre></div>' % (lineno, line))
-                stats['covered'] += 1
-            else:
-                rows.append('<div class="skip"><span class="num"><pre>%s</pre>'
-                            '</span><pre>%s</pre></div>' % (lineno, line))
-                stats['skipped'] += 1
-        stats['percent'] = self.computePercent(stats['covered'],
-                                               stats['missed'])
-        html = COVERAGE_TEMPLATE % {'title': '<title>%s</title>' % name,
-                                    'header': name,
-                                    'body': '\n'.join(rows),
-                                    'stats': COVERAGE_STATS_TEMPLATE % stats,
-                                   }
-        outfilename = name + '.html'
-        outfile = open(os.path.join(outputDir, outfilename), 'w')
-        outfile.write(html)
-        outfile.close()
-        return outfilename, stats
-
-    def computePercent(self, covered, missed):
-        if covered + missed == 0:
-            percent = 1
-        else:
-            percent = covered/(covered+missed+0.0)
-        return int(percent * 100)
+            self.coverInstance.html_report(modules, self.coverHtmlDir)
+        if self.coverXmlFile:
+            log.debug("Generating XML coverage report")
+            self.coverInstance.xml_report(modules, self.coverXmlFile)
 
     def wantModuleCoverage(self, name, module):
         if not hasattr(module, '__file__'):
